b233322d40ba84119921998ad269eb031d91c5e3
Joice Jessica <joicejssc@gmail.com>
2016-11-05 13:15:23 +0700

    SplashScreen

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/SplashActivity.java
@@ -0,0 +1,36 @@
+package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    startActivity(new Intent(SplashActivity.this, Home.class));
+                    finish();
+                }
+
+            }
+        };
+        thread.start();
+    }
+}
