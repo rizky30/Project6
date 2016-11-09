aec6b381369a8da0edc48b58fae35fba2322a401
Joice Jessica <joicejssc@gmail.com>
2016-11-09 21:55:10 +0700

    add Menu part 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/AksaraJawaMainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AksaraJawaMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aksara_jawa_main);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/Home.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/Home.java
@@ -1,10 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
 
 import android.app.Activity;
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 public class Home extends Activity
 {
@@ -14,6 +13,27 @@ public class Home extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findViewById(R.id.imageViewAksara).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, AksaraJawaMainActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageViewTembang).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, TembangMainActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageViewKewan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, KewanMainActivity.class));
+            }
+        });
 
     }
 }
