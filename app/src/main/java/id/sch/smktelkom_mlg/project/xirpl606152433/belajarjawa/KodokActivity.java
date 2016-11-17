e5d5ae4db94e2d80ae8222a99cede0903fbe49db
Joice Jessica <joicejssc@gmail.com>
2016-11-17 23:55:45 +0700

    addTembang part 1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/KodokActivity.java
@@ -0,0 +1,118 @@
+package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+
+
+public class KodokActivity extends Activity implements MediaPlayer.OnCompletionListener {
+    private Button play, pause, stop;
+    private MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kodok);
+
+
+        play = (Button) findViewById(R.id.buttonPlay);
+        pause = (Button) findViewById(R.id.buttonPause);
+        stop = (Button) findViewById(R.id.buttonStop);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+
+        setup();
+    }
+
+    private void pause() {
+        mp.pause();
+
+        play.setEnabled(true);
+        pause.setEnabled(false);
+        stop.setEnabled(true);
+    }
+
+    private void loadClip() {
+        try {
+            mp = MediaPlayer.create(this, R.raw.kodokngorek);
+            mp.setOnCompletionListener(this);
+        } catch (Throwable t) {
+            goBlooey(t);
+        }
+    }
+
+    private void goBlooey(Throwable t) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder
+                .setTitle("Peringatan")
+                .setMessage(t.toString())
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    private void setup() {
+        loadClip();
+        play.setEnabled(true);
+        pause.setEnabled(false);
+        stop.setEnabled(false);
+    }
+
+
+    private void stop() {
+        mp.stop();
+        pause.setEnabled(false);
+        stop.setEnabled(false);
+
+        try {
+            mp.prepare();
+            mp.seekTo(0);
+            play.setEnabled(true);
+        } catch (Throwable t) {
+            goBlooey(t);
+        }
+    }
+
+    private void play() {
+        mp.start();
+        play.setEnabled(false);
+        pause.setEnabled(true);
+        stop.setEnabled(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (stop.isEnabled()) {
+            stop();
+        }
+    }
+
+    public void onCompletion(MediaPlayer mp) {
+        stop();
+    }
+}
+
+
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/TembangMainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/TembangMainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TembangMainActivity extends Activity {
 
@@ -9,5 +11,33 @@ public class TembangMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tembang_main);
+
+        findViewById(R.id.imageViewKodok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TembangMainActivity.this, KodokActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageViewLir).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TembangMainActivity.this, LirActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageViewMenthok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TembangMainActivity.this, MenthokActivity.class));
+            }
+        });
+
+        findViewById(R.id.imageViewSluku).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TembangMainActivity.this, SlukuActivity.class));
+            }
+        });
     }
 }
