f473e3f1b48fe8a2c5d457efa4be1dcfcaecb75e
Muhammad Rizky Ajie Soelstyo <muhammad_soelistyo_24rpl@student.smktelkom-mlg.sch.id>
2016-11-20 23:56:23 +0700

    Kewan pt1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/GajahActivity.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GajahActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gajah);
+
+    }
+}
