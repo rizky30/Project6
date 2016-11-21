11d4da62db221b095193010ff273f205a1df8d32
Salsabila Putri Ray <salsabilaputrirayieningtyas@gmail.com>
2016-11-21 10:26:20 +0700

    Livecycle Basic

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/HaActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/HaActivity.java
@@ -2,12 +2,52 @@ package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HaActivity extends Activity {
+
+    private static final String TAG = "LivecycleTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ha);
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 }
