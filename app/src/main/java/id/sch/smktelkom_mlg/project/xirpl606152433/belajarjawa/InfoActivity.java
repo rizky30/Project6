13b25bb4b36642ae51e542206374446c09812821
Muhammad Rizky Ajie Soelstyo <muhammad_soelistyo_24rpl@student.smktelkom-mlg.sch.id>
2016-11-22 14:58:39 +0700

    Insert About Us

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/InfoActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/InfoActivity.java
@@ -1,52 +1,23 @@
 package id.sch.smktelkom_mlg.project.xirpl606152433.belajarjawa;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
-public class Home extends Activity
+public class InfoActivity extends Activity
 {
     private static final String TAG = "LivecycleTag";
     MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_info);
         Log.d(TAG, "onCreate: ");
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
-
-        findViewById(R.id.imageViewAksara).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, AksaraJawaMainActivity.class));
-            }
-        });
-
-        findViewById(R.id.imageViewTembang).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, TembangMainActivity.class));
-            }
-        });
-
-        findViewById(R.id.imageViewKewan).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, KewanMainActivity.class));
-            }
-        });
-
-        findViewById(R.id.imageViewInfo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, InfoActivity.class));
-            }
-        });
-
     }
 
     @Override
@@ -87,5 +58,4 @@ public class Home extends Activity
         Log.d(TAG, "onDestroy: ");
         if (mediaPlayer != null)
             mediaPlayer.release();
-    }
-}
+    }}
