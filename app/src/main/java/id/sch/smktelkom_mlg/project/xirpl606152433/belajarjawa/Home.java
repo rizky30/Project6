7f3dbbd511e587aff143a1725e57d6197968a4df
Salsabila Putri Ray <salsabilaputrirayieningtyas@gmail.com>
2016-12-07 22:35:49 +0700

    atur tampilan

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/Home.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl606152433/belajarjawa/Home.java
@@ -10,12 +10,14 @@ import android.view.View;
 public class Home extends Activity
 {
     private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaplayer;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: ");
+        mediaplayer = MediaPlayer.create(this, R.raw.awal);
 
         findViewById(R.id.imageViewAksara).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +47,45 @@ public class Home extends Activity
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+        mediaplayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+        mediaplayer.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+        if (mediaplayer != null)
+            mediaplayer.release();
     }
 }
